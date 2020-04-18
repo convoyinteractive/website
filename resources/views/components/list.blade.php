41050caf76806d463612f21b93e59db7884692f0
Tobias Thaden <hello@tobiasthaden.com>
2020-04-18 16:20:55 +0200

    Add list template

--- resources/views/components/list.blade.php
+++ resources/views/components/list.blade.php
@@ -1,3 +1,12 @@
-<div>
-    <pre>{{ var_dump($component) }}</pre>
-</div>
+<ul class="{{ $class ?? '' }}">
+    @foreach ($component->get('items', []) as $item)
+        <li>
+            @include(
+                view()->exists("components.{$item->type()}")
+                    ? "components.{$item->type()}"
+                    : "components.error",
+                ['component' => $item]
+            )
+        </li>
+    @endforeach
+</ul>
