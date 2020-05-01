37257ba11a557fade6d46964eb94def2840d26bc
Tobias Thaden <hello@tobiasthaden.com>
2020-05-01 21:09:19 +0200

    Update quote styles

--- resources/views/components/quote.blade.php
+++ resources/views/components/quote.blade.php
@@ -1,11 +1,11 @@
 <figure class="{{ $class ?? '' }}">
-    <div class="w-10/12 flex items-center flex-row-reverse mx-auto">
-        <blockquote class="block w-7/10 px-6">
+    <div class="lg:w-10/12 lg:flex items-center justify-between flex-row-reverse mx-auto">
+        <blockquote class="block lg:w-7/10 my-10 px-6 font-sans text-3xl xl:text-like-3xl leading-normal font-semibold">
             <p>{{ $component->get('body') }}</p>
         </blockquote>
-        <figcaption class="block w-3/10 px-6">
-            {{$component->get('author.name')}} <br>
-            {{ $component->get('author.title') }}
+        <figcaption class="block lg:w-2/10  my-10 px-6">
+            <span class="block font-sans text-lg xl:text-like-lg font-semibold">{{$component->get('author.name')}}</span>
+            <span class="block font-serif text-lg xl:text-like-lg">{{ $component->get('author.title') }}</span>
         </figcaption>
     </div>
 </figure>
