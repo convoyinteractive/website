927004b8d17ceb39b98c9e39f7239ef274aaec60
Tobias Thaden <hello@tobiasthaden.com>
2020-05-07 13:29:11 +0200

    Switch to lottie light

--- resources/js/Lottie.js
+++ resources/js/Lottie.js
@@ -6,7 +6,7 @@
  * @see https://github.com/imakewebthings/waypoints
  */
 
-import Airbnb from "lottie-web";
+import Airbnb from "lottie-web/build/player/lottie_light";
 import Waypoints from "./mixins/Waypoints";
 
 export default class Lottie {
