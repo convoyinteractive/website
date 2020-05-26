7cb48ff79d429d6e428fbae436875245ea7a77ad
Tobias Thaden <hello@tobiasthaden.com>
2020-05-26 16:41:47 +0200

    Remove service worker

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,38 +1,6 @@
 let Mix = require("laravel-mix");
-let Sw = require("sw-precache-webpack-plugin");
 
 Mix.setPublicPath("public");
-
-Mix.webpackConfig({
-    plugins: [
-        new Sw({
-            cacheId: "pwa",
-            filename: "service-worker.js",
-            staticFileGlobs: [
-                "public/**/*.{css,eot,svg,ttf,woff,woff2,js,html}",
-            ],
-            minify: true,
-            stripPrefix: "public/",
-            handleFetch: true,
-            dynamicUrlToDependencies: {
-                "/": ["resources/views/default.blade.php"],
-            },
-            staticFileGlobsIgnorePatterns: [
-                /\.map$/,
-                /mix-manifest\.json$/,
-                /manifest\.json$/,
-                /service-worker\.js$/,
-            ],
-            navigateFallback: "/",
-            runtimeCaching: [
-                {
-                    urlPattern: /^https:\/\/lib\.convoyinteractive\.com\//,
-                    handler: "cacheFirst",
-                },
-            ],
-        }),
-    ],
-});
 
 Mix.js("resources/js/app.js", "public/js");
 
