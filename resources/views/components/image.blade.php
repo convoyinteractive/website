fe7a4479e0c47b42e57c58be7f6d1f895b1e3082
Tobias Thaden <hello@tobiasthaden.com>
2020-09-23 11:04:16 +0200

    Allow single path images

--- resources/views/components/image.blade.php
+++ resources/views/components/image.blade.php
@@ -1,9 +1,13 @@
 <figure {{ $attributes ?? ''}} aria-label="{{ $component->get('alt') }}">
     <div class="max-h-full max-w-full">
+        @if($component->has('sizes'))
         <picture {{$content->attributes(['image.item'])}}>
             <source media="(min-width: 500px)" srcset="{{ asset($component->get('sizes.large'), []) }}">
             <img {{$content->attributes(['image.item'])}} src="{{ asset($component->get('sizes.small'), []) }}" alt="{{ $component->get('alt') }}">
         </picture>
+        @elseif($component->has('path'))
+            <img {{$content->attributes(['image.item'])}} src="{{ asset($component->get('path'), []) }}" alt="{{ $component->get('alt') }}">
+        @endif
     </div>
 
     @if($component->has('caption'))
--- resources/views/components/phone.blade.php
+++ resources/views/components/phone.blade.php
@@ -1,8 +1,12 @@
 <div {{ $attributes ?? ''}}>
     <phone>
+        @if($component->has('sizes'))
         <picture class="w-full">
             <source media="(min-width: 500px)" srcset="{{ asset($component->get('sizes.large'), []) }}">
             <img src="{{ asset($component->get('sizes.small'), []) }}" alt="{{ $component->get('alt') }}" class="w-full">
         </picture>
+        @elseif($component->has('path'))
+            <img class="w-full" {{$content->attributes(['image.item'])}} src="{{ asset($component->get('path'), []) }}" alt="{{ $component->get('alt') }}">
+        @endif
     </phone>
 </div>
