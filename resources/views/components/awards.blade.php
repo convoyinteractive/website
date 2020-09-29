29842b0feb23315612d48e5f9e7e1522b95f092f
Tobias Thaden <hello@tobiasthaden.com>
2020-09-29 22:37:54 +0200

    Update clients and awards sizes

--- resources/views/components/awards.blade.php
+++ resources/views/components/awards.blade.php
@@ -3,11 +3,11 @@
     <li {{ $content->attributes([$component->alias(), 'items']) }}>
         <div {{ $content->attributes([$component->alias(), 'card']) }}>
             <img
-                class="max-w-30 max-h-10"
+                class="max-w-30 max-h-15"
                 src="{{ asset($item->get('path'), [
                     'format' => 'png',
                     'portrait_height' => 240,
-                    'landscape_width' => 240,
+                    'landscape_width' => 400,
                 ]) }}"
                 alt="{{ $item->get('alt') }}"
             >
--- resources/views/components/clients.blade.php
+++ resources/views/components/clients.blade.php
@@ -3,11 +3,11 @@
     <li {{ $content->attributes([$component->alias(), 'items']) }}>
         <div {{ $content->attributes([$component->alias(), 'card']) }}>
             <img
-                class="max-w-30 max-h-15"
+                class="max-w-50 max-h-25"
                 src="{{ asset($item->get('path'), [
                     'format' => 'png',
                     'portrait_height' => 240,
-                    'landscape_width' => 240,
+                    'landscape_width' => 400,
                 ]) }}"
                 alt="{{ $item->get('alt') }}"
             >
