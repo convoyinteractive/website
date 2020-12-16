a98aa4e85f62de3fcccff13f8ce731c04a9b428d
Tobias Thaden <hello@tobiasthaden.com>
2020-12-16 16:19:38 +0100

    Remove case view

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -8,6 +8,10 @@
         'component' => $content->sections()->hero()
     ])
 
+    @foreach($content->sections()->aside() as $component)
+        @include($component->view())
+    @endforeach
+
     @foreach($content->sections()->body() as $component)
         @include($component->view())
     @endforeach
