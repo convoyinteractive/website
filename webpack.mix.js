54ec8af346dbab3646433cb21ec3d686f018d237
Tobias Thaden <hello@tobiasthaden.com>
2021-01-11 22:09:25 +0100

    Register "vue-template-compiler" with mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,7 @@ let Mix = require("laravel-mix");
 
 Mix.setPublicPath("public");
 
-Mix.js("resources/js/app.js", "public/js");
+Mix.js("resources/js/app.js", "public/js").vue({ version: 2 });
 
 Mix.postCss("resources/css/app.css", "public/css", [
     require("tailwindcss")("./tailwind.config.js"),
