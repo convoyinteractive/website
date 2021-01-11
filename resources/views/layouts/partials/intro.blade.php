f5e49f0e406a107268aaf5ee22de6f746f5a2790
Tobias Thaden <hello@tobiasthaden.com>
2021-01-11 13:17:33 +0100

    Conditionally render the intro section

--- resources/views/layouts/partials/intro.blade.php
+++ resources/views/layouts/partials/intro.blade.php
@@ -1,4 +1,6 @@
-<div {{ $content->attributes('intro.wrap') }}>
-    @include('layouts.partials.sidebar')
-    @include('layouts.partials.excerpt')
-</div>
+@if($content->has('aside') or $content->has('excerpt'))
+    <div {{ $content->attributes('intro.wrap') }}>
+        @include('layouts.partials.sidebar')
+        @include('layouts.partials.excerpt')
+    </div>
+@endif
