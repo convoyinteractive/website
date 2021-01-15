4aa0959dab4b28f48c89f2ea568c2a3fefe44f65
Tobias Thaden <hello@tobiasthaden.com>
2021-01-15 17:46:47 +0100

    Remove animation

--- resources/views/layouts/partials/contact.blade.php
+++ resources/views/layouts/partials/contact.blade.php
@@ -12,15 +12,9 @@
             </div>
             <div class="absolute flex items-center justify-center w-6 h-6 p-1 text-white text-center bg-green rounded-full right-0 bottom-0 -mr-2">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
-                    <circle fill="currentColor" stroke="none" cx="6" cy="26" r="6">
-                        <animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.1"/>
-                    </circle>
-                    <circle fill="currentColor" stroke="none" cx="26" cy="26" r="6">
-                        <animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.2"/>
-                    </circle>
-                    <circle fill="currentColor" stroke="none" cx="46" cy="26" r="6">
-                        <animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.3"/>
-                    </circle>
+                    <circle fill="currentColor" stroke="none" cx="6" cy="26" r="6" />
+                    <circle fill="currentColor" stroke="none" cx="26" cy="26" r="6" />
+                    <circle fill="currentColor" stroke="none" cx="46" cy="26" r="6" />
                 </svg>
             </div>
         </div>
