34a598a4a82dcceefbf299758d3d80b9437c3318
Tobias Thaden <hello@tobiasthaden.com>
2021-01-18 13:03:16 +0100

    Fix menu spacing

--- resources/views/layouts/partials/navigation.blade.php
+++ resources/views/layouts/partials/navigation.blade.php
@@ -44,7 +44,7 @@
                 <div ref="about" v-show="isActive('about')" class="flex flex-col justify-between min-h-full w-full space-y-20 pt-10 pb-30 md:px-20 md:pb-20">
                     <div>
                         @foreach($sitemap->collection('about.links', 'navigation') as $item)
-                        <a href="{{ $item->get('path') }}" class="block font-sans font-bold text-2xl md:text-3xl xl:text-like-3xl mb-5 md:mb-10 xl:mb-screen-10 mx-5 md:mx-15">
+                        <a href="{{ $item->get('path') }}" class="block font-sans font-bold text-2xl md:text-3xl xl:text-like-3xl mb-5 xl:mb-screen-5 mx-5 md:mx-15">
                             {{ $item->get('label') }}
                         </a>
                         @endforeach
