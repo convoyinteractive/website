f421aa87dc3e8b72fce9aee8ced49bb0387c2015
Tobias Thaden <hello@tobiasthaden.com>
2021-01-21 18:51:35 +0100

    Add apply links

--- resources/views/components/job.blade.php
+++ resources/views/components/job.blade.php
@@ -1,3 +1,6 @@
 @foreach ($component->get('body', []) as $item)
     @include($item->view(), ['component' => $item])
 @endforeach
+<a href="{{ encode_email('mailto:jobs@convoyinteractive.com') }}" class="mt-10 lg:mt-20 xl:mt-screen-20 font-sans font-semibold inline-flex flex-row-reverse items-center" v-button="['bg-white', 'text-black']">
+    <span>{{ __('Apply now') }}</span>
+</a>
