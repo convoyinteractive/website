67345534573023a96ad153af87fc964f697b41ee
Tobias Thaden <hello@tobiasthaden.com>
2021-01-26 14:55:04 +0100

    Add images in 2x to case cards

--- resources/views/components/case.blade.php
+++ resources/views/components/case.blade.php
@@ -1,22 +1,14 @@
 <a {{ $component->attributes('wrap') }} href="/{{ implode('/', [app('translator')->getLocale(), $component->get('resource')]) }}">
     <span {{ $component->attributes('image') }} data-theme='@json($component->theme())'>
         <picture class="w-full block">
-            <source type="image/webp"
-                srcset="{{ asset($component->get('meta.image'), [
-                    'width' => '640',
-                    'height' => '400',
-                    'format' => 'webp',
-                    'crop' => true,
-                ]) }}"
-            >
+            <source type="image/webp" srcset="
+                {{ asset($component->get('meta.image'), ['width' => '640', 'height' => '400', 'format' => 'webp', 'crop' => true ]) }},
+                {{ asset($component->get('meta.image'), ['width' => '1024', 'height' => '640', 'format' => 'webp', 'crop' => true ]) }} 2x
+            ">
             <img class="w-full block"
                 alt="{{ $component->get('meta.title') }}"
-                src="{{ asset($component->get('meta.image'), [
-                    'width' => '640',
-                    'height' => '400',
-                    'format' => 'jpg',
-                    'crop' => true,
-                ]) }}"
+                src="{{ asset($component->get('meta.image'), ['width' => '640', 'height' => '400', 'format' => 'jpg', 'crop' => true]) }}"
+                srcset="{{ asset($component->get('meta.image'), ['width' => '1024', 'height' => '640', 'format' => 'jpg', 'crop' => true ]) }} 2x"
             >
         </picture>
     </span>
