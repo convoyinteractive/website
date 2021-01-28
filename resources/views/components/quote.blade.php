279cd41da10ab8daef82925d5cce88df5695e347
Tobias Thaden <hello@tobiasthaden.com>
2021-01-28 11:50:13 +0100

    Add avatars to quotes

--- resources/views/components/quote.blade.php
+++ resources/views/components/quote.blade.php
@@ -4,6 +4,21 @@
             {{ $component->get('body') }}
         </div>
         <footer {{ $component->attributes('author') }}>
+            @if($component->has('author.image'))
+            <div {{ $component->attributes('avatar') }}>
+                <picture class="block w-full">
+                    <source type="image/webp"
+                        srcset="{{ asset($component->get('author.image'), ['format' => 'webp', 'width' => 200, 'height' => 200, 'crop' => true]) }},
+                            {{ asset($component->get('author.image'), ['format' => 'webp', 'width' => 400, 'height' => 400, 'crop' => true]) }} 2x"
+                    >
+                    <img class="block w-full"
+                        src="{{ asset($component->get('author.image'), ['format' => 'png', 'width' => 200, 'height' => 200, 'crop' => true]) }}"
+                        srcset="{{ asset($component->get('author.image'), ['format' => 'png', 'width' => 400, 'height' => 400, 'crop' => true]) }} 2x"
+                        alt="{{ $component->get('author.name') }}"
+                    >
+                </picture>
+            </div>
+            @endif
             <div>
                 <span {{ $component->attributes('author_name') }}>{{$component->get('author.name')}}</span>
                 <span {{ $component->attributes('author_title') }}>{{ $component->get('author.title') }}</span>
