290716207630c7718e721f395c4c15435885627f
Tobias Thaden <hello@tobiasthaden.com>
2021-06-08 17:10:32 +0200

    Fetch the excerpt in jobs listing item

--- resources/views/components/job.blade.php
+++ resources/views/components/job.blade.php
@@ -1,4 +1,4 @@
-@foreach ($component->get('body', []) as $item)
+@foreach ($component->get('excerpt', []) as $item)
     @include($item->view(), ['component' => $item])
 @endforeach
 <a href="{{ encode_email('mailto:jobs@convoyinteractive.com') }}" class="mt-10 lg:mt-20 xl:mt-screen-20 font-sans font-semibold inline-flex flex-row-reverse text-white items-center focus:outline-none border border-dotted border-transparent focus:border-black focus:border-opacity-25" v-button>
